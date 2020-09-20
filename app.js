6fb1b218bee66d89f3b82521f9b77c9276a11017
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-09-20 17:59:16 -0500

    added functions and refactured functions

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
+// CAN DELETE CODE - COMMENTED OUT AS REFERENCE:
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+// // Notice the lack of parentheses around the `profileDataArr` parameter?
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//       console.log(profileDataArr[i]);
+//     }
+
+//     console.log('================');
+
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+//   };
+
+// printProfileData(profileDataArgs);
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
 
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
+const [name, github] = profileDataArgs;
 
-    console.log('================');
-
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
-
-printProfileData(profileDataArgs);
+const generatePage = (userName, githubName) => {
+  return `
+  Name: ${userName}
+  Github: ${githubName}
+  `;
+}
+console.log(name, github);
+console.log(generatePage(name, github));
