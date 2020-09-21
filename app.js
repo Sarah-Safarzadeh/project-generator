ed7fccac416b5828ac2017d58d2926596a00d4ce
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-09-20 23:50:32 -0500

    added require var for fs module

--- app.js
+++ app.js
@@ -16,6 +16,8 @@
 
 // printProfileData(profileDataArgs);
 
+const fs = require('fs');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 const [name, github] = profileDataArgs;
@@ -38,5 +40,9 @@ const generatePage = (name, github) => {
   </html>
   `;
 };
-console.log(name, github);
-console.log(generatePage(name, github));
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
